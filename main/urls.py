11c71415232def3934cb425f236bf0fe923a0e9e
Subhanu Sankar Roy <71728589+SubhanuSRoy@users.noreply.github.com>
2022-11-05 01:15:32 +0530

    ft. made login page as default

--- main/urls.py
+++ main/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('home/',views.home,name='home'),
     path('books/',views.books,name='books'),
     path('register/',views.registerPage,name='register'),
-    path('login/',views.loginPage,name='login'),
+    path('',views.loginPage,name='login'),
     path('logout/',views.logoutUser,name='logout'),
     path('userPage/',views.userPage,name='userPage'),
     path('customer/<str:pk>',views.customer,name='customer'),
